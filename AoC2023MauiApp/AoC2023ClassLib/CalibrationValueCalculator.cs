91d7c92c00a45fbc0ff5217174451ab7e888cd0b
Heidstra <heidstra@gmail.com>
2023-12-03 12:57:06 +0100

    Day 1 solved

--- AoC2023MauiApp/AoC2023ClassLib/CalibrationValueCalculator.cs
+++ AoC2023MauiApp/AoC2023ClassLib/CalibrationValueCalculator.cs
@@ -23,10 +23,6 @@ namespace AoC2023ClassLib
             int sum = 0;
             foreach (string line in file)
             {
-                //string numbers = new string(line
-                //         //.SkipWhile(c => !char.IsDigit(c))
-                //         .TakeWhile(c => char.IsDigit(c))
-                //         .ToArray());
                 string numbers = "";
                 foreach (char character in line)
                 {
@@ -34,13 +30,53 @@ namespace AoC2023ClassLib
                     {
                         numbers += character;
                     };
-                }
-                
+                }                
 
                 string number = numbers.First().ToString() + numbers.Last().ToString();
                 sum += Convert.ToInt32(number);
             }
             CalibrationSum = sum;
+        }
+
+        public static void CalculateModified(string fileName)
+        {
+            string[] file = BestandHelper.Readfile(string.Format(@"Input\{0}.txt", fileName));
+            int sum = 0;
+
+            foreach (string line in file)
+            {
+                string numbers = "";
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if ((char.IsDigit(line[i])))
+                    {
+                        numbers += line[i];
+                    }
+                    else
+                    {
+                        numbers += foundDigitInText(line.Substring(i, Math.Min(line.Length-i,5)));
+                    }
+                }
+                string number = numbers.First().ToString() + numbers.Last().ToString();
+                sum += Convert.ToInt32(number);
+            }
+
+            CalibrationSum = sum;
+        }
+
+        private static string foundDigitInText(string text)
+        {
+            if (text.StartsWith("one")) { return "1"; }
+            if (text.StartsWith("two")) { return "2"; }
+            if (text.StartsWith("three")) { return "3"; }
+            if (text.StartsWith("four")) { return "4"; }
+            if (text.StartsWith("five")) { return "5"; }
+            if (text.StartsWith("six")) { return "6"; }
+            if (text.StartsWith("seven")) { return "7"; }
+            if (text.StartsWith("eight")) { return "8"; }
+            if (text.StartsWith("nine")) { return "9"; }
+
+            return "";
         }
 
         public static int CalibrationSum { get; private set; }
